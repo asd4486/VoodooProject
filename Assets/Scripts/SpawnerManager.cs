c3c0d4392b729cbe8d268568509a8264f8a7561d
giinsen <gabriel.lapegue@yahoo.fr>
2019-07-11 12:14:30 +0200

    environment

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject walls;
+    public GameObject wallsSpawner;
+    public GameObject wall;
+
+    private void Update()
+    {
+        walls.transform.Translate(Vector3.right * GameManager.Instance.environmentSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.tag == "Wall")
+        {
+            Instantiate(wall, wallsSpawner.transform.position, Quaternion.identity, walls.transform);
+        }
+    }
+}
